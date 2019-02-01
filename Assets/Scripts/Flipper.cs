624319822ef569d415777f90b62f9abf335c37ce
Agentkow <kennethkhau212@gmail.com>
2019-01-31 18:40:08 -0600

    Git hub upload

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    [SerializeField]
+    private float restPos = 0f;
+    [SerializeField]
+    private float pressedPos = 45f;
+    [SerializeField]
+    private float hitStrength = 100000f;
+    private float flipDamp = 150f;
+
+    private HingeJoint hinge;
+
+    [SerializeField]
+    private string inputName;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipDamp;
+
+        if (Input.GetButton(inputName))
+        {
+            spring.targetPosition = pressedPos;
+        }
+        else
+        {
+            spring.targetPosition = restPos;
+        }
+
+        hinge.spring = spring;
+        hinge.useLimits = true;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform startTransmform;
+    private Transform form;
+
+    [SerializeField]
+    private float speed=100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        form = this.transform;
+        startTransmform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MoveBar(new Vector2(Input.GetAxis("Horizontal"), 0));
+        
+    }
+
+    void MoveBar(Vector2 direction)
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+}
