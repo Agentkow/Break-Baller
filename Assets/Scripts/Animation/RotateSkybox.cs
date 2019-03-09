0a225cd56d93ab85798af9eef7d67f3bbc2fdfe0
Agentkow <kennethkhau212@gmail.com>
2019-03-08 18:19:01 -0600

    no water, no tiny trees, minor art tweeks, and rotating skybox

---
+++ Assets/Scripts/Animation/RotateSkybox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+    }
+}
