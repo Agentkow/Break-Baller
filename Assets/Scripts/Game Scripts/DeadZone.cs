caf071264347995c9c0bb2b2dbc020e581434445
Agentkow <kennethkhau212@gmail.com>
2019-03-15 15:57:20 -0500

    ball lose and health lose audio

--- Assets/Scripts/Game Scripts/DeadZone.cs
+++ Assets/Scripts/Game Scripts/DeadZone.cs
@@ -11,18 +11,27 @@ public class DeadZone : MonoBehaviour
     [SerializeField]
     private AudioSource gameOverSound;
 
+    [SerializeField]
+    private AudioSource ballLossAudio;
+
+    [SerializeField]
+    private AudioSource tikiLossAudio;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
+            ballLossAudio.Play();
             Destroy(other.gameObject);
         }
 
         if (other.gameObject.CompareTag("Enemy"))
         {
             manager.health--;
+            tikiLossAudio.Play();
             Destroy(other.gameObject);
         }
+
         if (manager.health == 0 || manager.ballLives == 0)
         {
             gameOverSound.Play();
